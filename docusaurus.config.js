19248e82f43a77932e1d64188fbeddca9fa0326d
Sam Henry <thesamueljim@gmail.com>
2022-06-27 10:09:18 +1000

    main not master

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,13 +60,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/samueljim/wiki/edit/master/website/',
+            'https://github.com/samueljim/wiki/edit/main/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/samueljim/wiki/edit/master/website/blog/',
+            'https://github.com/samueljim/wiki/edit/main/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
